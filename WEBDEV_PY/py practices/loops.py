acb7fd7fde98b35e7993a09242e60836a96bf8b4
d1p3nbhattara1 <dipenbhattarai15@gmail.com>
2024-01-28 19:53:10 +0545

    Dictionaries and loops
    learning about dictionaries and loops

---
+++ WEBDEV_PY/py practices/loops.py
@@ -0,0 +1,32 @@
+#loop in python
+#CREATING A SIMPLE LOOP
+for i in [1,2,3,4,5]:
+    print(i)
+
+print('''
+
+''')
+
+for i in range(100):
+    print(i)
+
+print('''
+     
+''')
+
+
+
+#working with loops in python and lists
+
+name=["lassi","energy","drink"]
+for i in name:
+    print(i)
+
+print('''
+
+ ''')
+
+
+#string and loops in python
+name=""
+
---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+# creating a dict
+# syntax:
+# dict_name={"key":"value"}
+# to access a dict:
+# print(dict_name["key"])
+
+address={"Dipen":"ramkot","Sushant":"swoyambhu","Bishal":"lalitput"}
+print(address["Dipen"])
+#prints ramkot for dipen
+
+#adding new keys and value :
+address["Krish"]="Balaju"
+print(address["Krish"])
